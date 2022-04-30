2887e2e7a8b7d735d6983e28f1c5d9244b7ba28b
unknown <omgodage@gmail.com>
2022-04-30 22:42:44 +0530

    edited PCA_MNIST

--- PCA_MNIST/revised PCA_MNIST.py
+++ PCA_MNIST/revised PCA_MNIST.py
@@ -11,11 +11,12 @@ data.drop('label', axis=1, inplace=True)
 i = int(input("Enter a number between 0 and 41999, and I'll serve you with a number: "))
 image = data.iloc[i].values.reshape([28, 28])
 plt.imshow(image, cmap='gray_r')
-plt.title('Actual image '+str(i)+': Digit '+str(label[i]), fontsize=15, pad=15)
+plt.title('Actual image '+str(i)+': Digit '+str(label[i]) + str(len(image)), fontsize=15, pad=15)
 
 
 pca_100 = PCA(n_components=100)
 mnist_pca_100_reduced = pca_100.fit_transform(data)
+print(len(mnist_pca_100_reduced[i, :]))
 mnist_pca_100_recovered = pca_100.inverse_transform(mnist_pca_100_reduced)
 # np.cumsum(pca_100.explained_variance_ratio_ * 100)[-1] shows the percent of variance compared to the original image
 
