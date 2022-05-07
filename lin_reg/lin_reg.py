3949fcf089aadc54e090ab1cdfa857505339e168
unknown <omgodage@gmail.com>
2022-05-07 22:37:22 +0530

    modified lin_reg

--- lin_reg/lin_reg.py
+++ lin_reg/lin_reg.py
@@ -6,7 +6,6 @@ rate = 0.0001
 
 def normal():
     theta_new = np.matmul(np.matmul(np.linalg.pinv(np.matmul(model.T, model)), model.T), Y)
-    print("The loss using the normal equations method: ", cost(theta_new))
 
 
 def h(row, column):
@@ -39,14 +38,22 @@ def grad_desc(theta_new):
         theta_new = theta_new - (rate*(np.matmul(model.T, g(theta_new).copy() - Y))/model.shape[0])
         old_cost = new_cost
         new_cost = cost(theta_new)
-    print("The loss via gradient descent:", new_cost)
+    return theta_new
 
 
 data = pd.read_csv("data.csv")
 regions = data['Region']
 model = pd.DataFrame(data, columns=['Temperature (T)', 'Rainfall (mm)', 'Humidity (%)']).values
 model = np.hstack((np.ones((15, 1)), model))
-Y = pd.DataFrame(data, columns=['Oranges (ton)']).values
 theta = np.zeros((model.shape[1], 1))
-# grad_desc(theta)
-# normal()
+
+
+temp, rain, humid = input("enter the temperature(F), Rainfall(mm) and Humidity(%): ").split()
+x = [1, float(temp), float(rain), float(humid)]
+Y = pd.DataFrame(data, columns=['Mangoes (ton)']).values
+theta = grad_desc(theta)
+Mango_pred = h(x, theta)
+Y = pd.DataFrame(data, columns=['Oranges (ton)']).values
+theta = grad_desc(theta)
+Orange_pred = h(x, theta)
+print("The orange production will be around", Orange_pred, "tons,\n and the mango production around", Mango_pred, 'tons')
