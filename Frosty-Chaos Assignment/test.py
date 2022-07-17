cb43826200b7ed52931dd794935ab0fab434b9fc
Om Godage <93699281+0-JackFrost-0@users.noreply.github.com>
2022-07-17 10:27:31 +0530

    corrected test.py

--- Frosty-Chaos Assignment/test.py
+++ Frosty-Chaos Assignment/test.py
@@ -1,51 +1,55 @@
-# Use this to test your model. 
-# Use appropriate command line arguments and conditions
-
 import argparse
+
 import torch
-from metrics import calc_psnr
+import torch.backends.cudnn as cudnn
+import numpy as np
+import PIL.Image as pil_image
+
 from model import Net
-from tqdm import tqdm
-from datasets import EvalDataset
-import matplotlib.pyplot as plt
+from preprocess import convert_rgb_to_ycbcr, convert_ycbcr_to_rgb
+from metrics import calc_psnr
 
-CUDA = torch.cuda.is_available()
-device = torch.device('cuda' if CUDA else 'cpu')
 
-batch_size = 4
-test_dataset = EvalDataset('./testdata/Set5_x2.h5')
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
-avg_psnr = 0
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights-file', type=str, required=True)
+    parser.add_argument('--image-file', type=str, required=True)
+    parser.add_argument('--scale', type=int, default=3)
+    args = parser.parse_args()
 
-model = torch.load('./model_path.pth')
-model.eval()
+    cudnn.benchmark = True
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-with torch.no_grad():
-    # with tqdm(total=len(test_loader)) as p_bar:
-    #     for batch_num, (data, target) in enumerate(test_loader):
-    #         pass
-    data, target = torch.from_numpy(test_dataset.__getitem__(1)[0]), torch.from_numpy(test_dataset.__getitem__(1)[1])
-    print(data.shape, target.shape)
-    prediction = model(data)
-    # print('PSNR of LR with HR:', calc_psnr(data, target))
-    print('PSNR is coming out to be: ', calc_psnr(target, prediction))
-    avg_psnr += calc_psnr(prediction, target)
-    # p_bar.update(1)
+    model = torch.load(args.weights_file)
 
-    fig = plt.figure()
-    fig.add_subplot(2, 2, 1)
-    plt.imshow(torch.squeeze(data))
-    plt.title('LR')
-    plt.axis('off')
-    fig.add_subplot(2, 2, 2)
-    plt.imshow(torch.squeeze(target))
-    plt.title('HR')
-    plt.axis('off')
-    fig.add_subplot(2, 2, 3)
-    plt.imshow(torch.squeeze(prediction))
-    plt.title('Predicted')
-    plt.axis('off')
+    model.eval()
 
-    plt.show()
-    print("PSNR: {:.4f} dB".format(avg_psnr))
+    image = pil_image.open(args.image_file).convert('RGB')
 
+    image_width = (image.width // args.scale) * args.scale
+    image_height = (image.height // args.scale) * args.scale
+    image = image.resize((image_width, image_height), resample=pil_image.Resampling.BICUBIC)
+    image = image.resize((image.width // args.scale, image.height // args.scale), resample=pil_image.Resampling.BICUBIC)
+    image = image.resize((image.width * args.scale, image.height * args.scale), resample=pil_image.Resampling.BICUBIC)
+    image.save(args.image_file.replace('.', '_bicubic_x{}.'.format(args.scale)))
+
+    image = np.array(image).astype(np.float32)
+    ycbcr = convert_rgb_to_ycbcr(image)
+
+    y = ycbcr[..., 0]
+    y /= 255.
+    y = torch.from_numpy(y).to(device)
+    y = y.unsqueeze(0).unsqueeze(0)
+
+    with torch.no_grad():
+        preds = model(y).clamp(0.0, 1.0)
+
+    psnr = calc_psnr(y, preds)
+    print('PSNR: {:.2f}'.format(psnr))
+
+    preds = preds.mul(255.0).cpu().numpy().squeeze(0).squeeze(0)
+
+    output = np.array([preds, ycbcr[..., 1], ycbcr[..., 2]]).transpose([1, 2, 0])
+    output = np.clip(convert_ycbcr_to_rgb(output), 0.0, 255.0).astype(np.uint8)
+    output = pil_image.fromarray(output)
+    output.save(args.image_file.replace('.', '_srcnn_x{}.'.format(args.scale)))
